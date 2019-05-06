2df8acf9f52b8be541f1750b41879644c35fabd4
Antony <amwaniki180@gmail.com>
2019-05-06 12:46:53 +0300

    add all

---
+++ js/style.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+$("#image1").click(function(){
+$("#design").show();
+$("#image1").hide();
+
+$("#design").click(funnction)(){
+  $("#image1").show();
+  $("#design").hide();
+});
+});
+});
+$(document).ready(function(){
+$("#image2").click(function(){
+$("#devel").show();
+$("#image2").hide();
+
+$("#devel").click(funnction)(){
+  $("#image2").show();
+  $("#devel").hide();
+});
+});
+});
+
+$(document).ready(function(){
+$("#image3").click(function(){
+$("#product").show();
+$("#image3").hide();
+
+$("#product").click(funnction)(){
+  $("#image3").show();
+  $("#product").hide();
+});
+});
+});
